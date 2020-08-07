de68d520bd47ac75b35ec8e6db9a3e74073ceac1
Arpit Bharti <arpitbharti73@gmail.com>
2020-08-07 07:48:01 +0530

    Update title

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -4,7 +4,7 @@ const Meta: React.FC = () => {
     return (
         <Head>
             <meta charSet="utf-8" />
-            <title>Arpit Bharti</title>
+            <title>Arpit Bharti - Software Developer</title>
             <meta name="theme-color" content="rgb(30,30,30)" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="shortcut icon" href="/favicon.ico" />
