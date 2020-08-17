f438977e66c0c7cf54191d273f81755dcd37879b
Arpit Bharti <arpitbharti73@gmail.com>
2020-08-18 03:38:44 +0530

    Remove about me and add blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,19 @@
-import styled from '@emotion/styled';
-
 import AboutMe from '../components/AboutMe/AboutMe';
+import Blogs from '../components/Blogs/Blogs';
 import Experience from '../components/Experience/Experience';
 import Intro from '../components/Intro/Intro';
 import Projects from '../components/Projects/Projects';
 import Skills from '../components/Skills/Skills';
 
-// const Space = styled.div`
-//     height: 70px;
-//     width: 100%;
-//     position: relative;
-//     top: 0px;
-//     padding-top: 0px;
-//     padding-bottom: 3cm;
-//     background-color: transparent;
-//     color: #f9f9f9;
-// `;
-
 const Home: React.FC = () => {
     return (
         <>
             <Intro />
-            <AboutMe />
+            {/* <AboutMe /> */}
             <Experience />
             <Skills />
             <Projects />
+            <Blogs />
         </>
     );
 };
