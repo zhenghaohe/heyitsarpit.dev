1299241b0d3b0bd1c5c161ca28351e92059fe250
Arpit Bharti <arpitbharti73@gmail.com>
2020-08-18 03:43:56 +0530

    new skills diplay format

--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -11,31 +11,41 @@ const Content = styled.div`
     flex-direction: row;
     justify-content: space-between;
     align-content: space-between;
+    padding-top: 5px;
 `;
 
 const List = styled.ul`
     display: flex;
     flex-direction: column;
+
+    width: 100%;
+
     list-style: none;
     margin: 0;
     padding: 0;
 `;
 
 const ListItem = styled.li`
-    padding: 0.5em 0;
-    transition: all 0.5s;
-    position: relative;
+    padding: 0.5em;
     font-size: 0.8rem;
 
-    /* &:hover {
-        padding-bottom: 0.05rem;
-    } */
+    width: 100%;
+
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+`;
+
+const Item = styled.span`
+    padding: 0.5em;
+    font-size: 0.8rem;
 `;
 
 const Data = [
     ['JavaScript', 'TypeScript', 'HTML', 'CSS', 'Python', 'C++'],
-    ['ReactJS', 'Redux', 'React Router', 'NextJS', 'SASS', 'Styled Components'],
-    ['NodeJS', 'Express', 'MongoDB', 'AWS', 'PostgreSQL', 'TypeORM'],
+    ['ReactJS', 'Redux', 'React Router', 'NextJS', 'SASS', 'Styled-Components'],
+    ['NodeJS', 'Express', 'MongoDB', 'AWS EC2 | S3', 'PostgreSQL', 'TypeORM'],
     ['Git', 'Mercurial', 'Bash']
 ];
 
@@ -44,13 +54,15 @@ const Skills: React.FC = () => {
         <SkillsStyled id="skills">
             <Heading href="#skills">Skills</Heading>
             <Content>
-                {Data.map((list) => (
-                    <List key={list[0]}>
-                        {list.map((item) => (
-                            <ListItem key="item">{item}</ListItem>
-                        ))}
-                    </List>
-                ))}
+                <List>
+                    {Data.map((list) => (
+                        <ListItem key={list[0]}>
+                            {list.map((item) => (
+                                <Item key={item}>{item}</Item>
+                            ))}
+                        </ListItem>
+                    ))}
+                </List>
             </Content>
         </SkillsStyled>
     );
