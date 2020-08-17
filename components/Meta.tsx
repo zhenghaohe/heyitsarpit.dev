7951e0e29170a68a27535b43d4377018f75505d9
Arpit Bharti <arpitbharti73@gmail.com>
2020-08-18 03:37:16 +0530

    New Font

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -5,12 +5,19 @@ const Meta: React.FC = () => {
         <Head>
             <meta charSet="utf-8" />
             <title>Arpit Bharti - Software Developer</title>
+            <meta
+                name="description"
+                content="Young and bright eyed software developer from India"
+            />
             <meta name="theme-color" content="rgb(30,30,30)" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="shortcut icon" href="/favicon.ico" />
             <link rel="stylesheet" type="text/css" href="/styles/normalize.css" />
             <link rel="stylesheet" type="text/css" href="/styles/global.css" />
-            <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet" />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;700;900&display=swap"
+                rel="stylesheet"
+            />
         </Head>
     );
 };
