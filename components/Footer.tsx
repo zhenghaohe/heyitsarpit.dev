f3d8ed236ba107c695f154a85e4930ca13c7f47a
Arpit Bharti <arpitbharti73@gmail.com>
2020-08-18 03:36:32 +0530

    New footer mechanism

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,33 +21,33 @@ const FooterStyled = styled.div`
     }
 `;
 
+const Links = [
+    'https://www.youtube.com/watch?v=3bNITQR4Uso', // Plastic Love
+    'https://www.youtube.com/watch?v=-ed6UeDp1ek', // Let it happen
+    'https://www.youtube.com/watch?v=xUNqsfFUwhY', // Here comes the sun
+    'https://www.youtube.com/watch?v=NvsQPCbgJOA', // Bodys
+    'https://www.youtube.com/watch?v=826mxiKjoP0', // The adults are talking
+    'https://www.youtube.com/watch?v=Ql_dEdMEjl4', // Flowers
+    'https://www.youtube.com/watch?v=8ECPu3iumnE', // Mystery of love
+    'https://www.youtube.com/watch?v=KmpzIU301VU', // Separator
+    'https://www.youtube.com/watch?v=c_-cUdmdWgU' // Chicago
+];
+
 const Footer: React.FC = () => {
     const [clicked, setClicked] = useState(false);
+    const [randomIndex, setRandomIndex] = useState(Math.floor(Math.random() * Links.length));
 
-    const Links = [
-        'https://www.youtube.com/watch?v=3bNITQR4Uso', // Plastic Love
-        'https://www.youtube.com/watch?v=-ed6UeDp1ek', // Let it happen
-        'https://www.youtube.com/watch?v=xUNqsfFUwhY', // Here comes the sun
-        'https://www.youtube.com/watch?v=NvsQPCbgJOA', // Bodys
-        'https://www.youtube.com/watch?v=826mxiKjoP0', // The adults are talking
-        'https://www.youtube.com/watch?v=Ql_dEdMEjl4', // Flowers
-        'https://www.youtube.com/watch?v=8ECPu3iumnE', // Mystery of love
-        'https://www.youtube.com/watch?v=KmpzIU301VU', // Separator
-        'https://www.youtube.com/watch?v=c_-cUdmdWgU' // Chicago
-    ];
-
-    // let randomIndex = Links[Math.floor(Math.random() * Links.length)];
-    // const onClick = () => {
-    //     setClicked(true);
-
-    // }
+    const onClick = () => {
+        setClicked(true);
+        setRandomIndex(Math.max(randomIndex + 1, 0));
+    };
 
     return (
         <FooterStyled id="footer">
             <a
-                href={Links[Math.floor(Math.random() * Links.length)]}
+                href={Links[randomIndex]}
                 target="_blank"
-                onClick={() => setClicked(true)}
+                onClick={onClick}
                 rel="noopener noreferrer">
                 {!clicked ? 'Click Me!' : 'Click Me Again!!'}
             </a>
