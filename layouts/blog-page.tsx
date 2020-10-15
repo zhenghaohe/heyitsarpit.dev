9c7d8e66a31fd28bb236a029103a79a6a6b3c08c
Arpit Bharti <arpitbharti73@gmail.com>
2020-10-15 21:22:50 +0530

    Fix layouts

--- layouts/blog-page.tsx
+++ layouts/blog-page.tsx
@@ -1,27 +1,26 @@
 import { NextSeo } from 'next-seo';
 
-import TagList from '../components/Tags';
 import formatDate from '../utils/formatDate';
 import { Article, DateAndReadTime, Description, Separator, Title } from './Styles/blog';
 
-export default function Layout(frontMatter: FrontMatter) {
-  return function Content({ children: content }: { children: JSX.Element }): JSX.Element {
-    return (
-      <>
-        <NextSeo title={frontMatter.title} description={frontMatter.description} />
-        <Article>
-          <Title>{frontMatter.title}</Title>
-          <DateAndReadTime>
-            {formatDate(frontMatter.date)}
-            <Separator>•</Separator>
-            {frontMatter.readingTime.text}
-          </DateAndReadTime>
-          {/* <img src={frontMatter.cover_image} alt="cover" /> */}
-          <Description>{frontMatter.description}</Description>
-          {content}
-          {/* <TagList tags={frontMatter.tags} /> */}
-        </Article>
-      </>
-    );
-  };
-}
+type Props = { frontMatter: FrontMatter };
+
+const Layout: React.FC<Props> = ({ frontMatter, children }) => {
+  return (
+    <>
+      <NextSeo title={frontMatter.title} description={frontMatter.description} />
+      <Article>
+        <Title>{frontMatter.title}</Title>
+        <DateAndReadTime>
+          {formatDate(frontMatter.date)}
+          <Separator>•</Separator>
+          {frontMatter.readingTime.text}
+        </DateAndReadTime>
+        <Description>{frontMatter.description}</Description>
+        {children}
+      </Article>
+    </>
+  );
+};
+
+export default Layout;
