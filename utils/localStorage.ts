0cc2ed67718bbfe9da919064b62a185bd6ae94c8
Arpit Bharti <arpitbharti73@gmail.com>
2020-12-06 21:27:56 +0530

    Fix window Reference Errors

--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -1,8 +1,10 @@
 export const setLocalStorage = <T>(key: string, data: T): void => {
   try {
-    if (window !== undefined) {
-      window.localStorage.setItem(key, JSON.stringify(data));
+    if (typeof window === undefined) {
+      return;
     }
+
+    window.localStorage.setItem(key, JSON.stringify(data));
   } catch (err) {
     console.log(err);
   }
@@ -10,15 +12,15 @@ export const setLocalStorage = <T>(key: string, data: T): void => {
 
 export const getLocalStorage = <T>(key: string, initialValue: T): T => {
   try {
-    if (window !== undefined) {
-      const storedData = window.localStorage.getItem(key);
-      if (storedData) {
-        return JSON.parse(storedData);
-      } else {
-        setLocalStorage(key, initialValue);
-        return initialValue;
-      }
+    if (typeof window === undefined) {
+      return initialValue;
+    }
+
+    const storedData = window.localStorage.getItem(key);
+    if (storedData) {
+      return JSON.parse(storedData);
     } else {
+      setLocalStorage(key, initialValue);
       return initialValue;
     }
   } catch (err) {
