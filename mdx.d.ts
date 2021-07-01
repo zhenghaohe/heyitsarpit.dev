3945fb549d49cdbc618877bce9e4b0daf6eb536b
Arpit Bharti <arpitbharti73@gmail.com>
2021-07-01 22:02:25 +0530

    Update global types

--- mdx.d.ts
+++ mdx.d.ts
@@ -1,24 +1,11 @@
-interface FrontMatter {
-  __resourcePath: string;
-  layout: string;
+interface PostMeta {
   title: string;
-  published: string;
+  published: boolean;
+  date: string;
   description: string;
   tags: string;
   cover_image: string;
   cover_artist: string;
   cover_artist_url: string;
-  date: string;
-  readingTime: {
-    text: string;
-    time: number;
-    words: number;
-    minutes: number;
-  };
-}
-
-declare module '*.mdx' {
-  let MDXComponent: (props: unknown) => JSX.Element;
-  export default MDXComponent;
-  export const frontMatter: FrontMatter[];
+  slug: string;
 }
