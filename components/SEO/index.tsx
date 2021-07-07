d1312b477bb5a6102a86ccadbd6ed6dd15898ce5
Arpit Bharti <arpitbharti73@gmail.com>
2021-07-07 20:58:42 +0530

    - Replace seo config with component - Major css reactor, move blog css to scss+tailwind

---
+++ components/SEO/index.tsx
@@ -0,0 +1,38 @@
+import { DefaultSeo } from 'next-seo';
+
+export const SEO: React.FC = () => {
+  return (
+    <DefaultSeo
+      title="Hi, I'm Arpit"
+      description="Software Developer"
+      canonical="https://heyitsarpit.dev/"
+      openGraph={{
+        url: 'https://heyitsarpit.dev/',
+        title: 'Arpit',
+        description: 'Software Developer',
+        site_name: 'heyitsarpit'
+        // images: [
+        //   {
+        //     url: 'https://www.example.ie/og-image-01.jpg',
+        //     width: 800,
+        //     height: 600,
+        //     alt: 'Og Image Alt'
+        //   },
+        //   {
+        //     url: 'https://www.example.ie/og-image-02.jpg',
+        //     width: 900,
+        //     height: 800,
+        //     alt: 'Og Image Alt Second'
+        //   },
+        //   { url: 'https://www.example.ie/og-image-03.jpg' },
+        //   { url: 'https://www.example.ie/og-image-04.jpg' }
+        // ]
+      }}
+      twitter={{
+        handle: '@onygami',
+        site: '@onygami',
+        cardType: 'summary_large_image'
+      }}
+    />
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,13 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 import '../public/styles/font.css';
-import '../public/styles/global.css';
+import '../public/styles/global.scss';
 
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { DefaultSeo } from 'next-seo';
 
 import Layout from '../components/Layout';
-import SEO from '../next-seo.config';
+import { SEO } from '../components/SEO';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
@@ -18,7 +17,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <DefaultSeo {...SEO} />
+      <SEO />
       <Layout>
         <Component {...pageProps} />
       </Layout>
