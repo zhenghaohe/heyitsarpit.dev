8cd64d3680a0ded0ebce1f2c5b3a4a08be597539
Arpit Bharti <arpitbharti73@gmail.com>
2021-09-23 20:16:10 +0530

    Added Sitemap support to the website

---
+++ next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: process.env.VERCEL_URL || 'https://heyitsarpit.dev',
+  generateRobotsTxt: true
+};
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import Document, { Head, Html, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
@@ -10,7 +9,7 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="/images/icon-48x48.png" />
           <link rel="apple-touch-icon" href="/images/icon-192x192.png" />
           <link rel="manifest" href="/manifest.json" />
-          <Script async src="https://cdn.splitbee.io/sb.js" />
+          <script async src="https://cdn.splitbee.io/sb.js" />
         </Head>
         <body className="dark">
           <script
