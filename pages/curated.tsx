77b7e355047f1cdd4ef303b2fef368b9f14d5f86
Arpit Bharti <arpitbharti73@gmail.com>
2021-09-23 11:25:51 +0530

    Added example for typefaces and reveal animations

--- pages/curated.tsx
+++ pages/curated.tsx
@@ -1,19 +1,31 @@
-const Font = ['Inter', 'iA Quattro', 'Garamond', 'ET book', 'JetBrains Mono'];
+const TypefaceList = ['Inter', 'iA Quattro', 'Cormorant Garamond', 'ET Book', 'JetBrains Mono'];
+
+function Typefaces() {
+  return (
+    <section>
+      <h2>Typefaces</h2>
+      <ul className="grid grid-cols-1 sm:grid-cols-3">
+        {TypefaceList.map((typeface) => (
+          <li
+            key={typeface}
+            style={{ fontFamily: typeface }}
+            className="relative flex items-center p-6 my-6 text-lg rounded-md group before:contents">
+            <p className="absolute h-full transition-opacity group-hover:opacity-0">{typeface}</p>
+            <p className="absolute transition-all translate-y-4 opacity-0 group-hover:translate-y-0 group-hover:opacity-100">
+              You reading an example for the typeface {typeface}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
 
 export default function Curated() {
   return (
     <article className="w-full sm:max-w-[75ch] mx-auto px-5 py-16">
       <h1 className="text-2xl font-black">A list of web things I love</h1>
-      <section>
-        <h2>Typefaces</h2>
-        <section>
-          {Font.map((font) => (
-            <div key={font} className="p-6 my-8 text-xl text-black bg-white rounded-md">
-              {font}
-            </div>
-          ))}
-        </section>
-      </section>
+      <Typefaces />
     </article>
   );
 }
