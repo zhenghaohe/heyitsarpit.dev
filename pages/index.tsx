974c6a46b8e533a1c751d304081ee61d29e3fee5
Arpit Bharti <arpitbharti73@gmail.com>
2021-10-16 16:20:36 +0530

    Updated home page, now pulls data from .mdx file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,81 +1,33 @@
-import Link from 'next/link';
+import fs from 'fs';
+import { getMDXComponent } from 'mdx-bundler/client';
+import { InferGetStaticPropsType } from 'next';
+import path from 'path';
+import { useMemo } from 'react';
 
-const Social = () => (
-  <section className="mt-16">
-    <h2 className="font-semibold">Find Me Online</h2>
-    <ul>
-      {[
-        { name: 'Github', url: 'https://github.com/heyitsarpit' },
-        { name: 'LinkedIn', url: 'https://www.linkedin.com/in/arpitbharti' },
-        { name: 'Twitter', url: 'https://twitter.com/heyitsarpit' }
-      ].map(({ name, url }) => (
-        <li key={name} className="flex items-center pl-0 max-w-max before:hidden">
-          <a
-            className="flex items-center hover:link"
-            href={url}
-            rel="noopener noreferrer"
-            target="_blank">
-            {name}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-4 h-4 ml-1"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-              />
-            </svg>
-          </a>
-        </li>
-      ))}
-    </ul>
-  </section>
-);
+import { Experience } from '@/components/Experience';
+import { components } from '@/components/MDXComponents';
+import { Projects } from '@/components/Projects';
+import { loadMDX } from '@/utils/loadMDX';
 
-const Home: React.FC = () => {
-  return (
-    <div className="w-full sm:max-w-[75ch] mx-auto h-full px-5 flex justify-center items-center">
-      <article className="w-full">
-        <h1 className="mb-3 text-3xl font-semibold">Hi, I&apos;m Arpit</h1>
-        <p>
-          Software Developer.
-          <Link href="/posts">
-            <a className="link">Writing</a>
-          </Link>{' '}
-          about Programming and Web Development.
-          <br />
-          Making some{' '}
-          <Link href="/art">
-            <a className="link">art. </a>
-          </Link>
-          Working at{' '}
-          <a
-            className="link"
-            href="https://www.linkedin.com/company/antillia-llc/"
-            target="_blank"
-            rel="noreferrer">
-            Antillia.
-          </a>
-        </p>
-        <div className="mt-12">
-          <Link href="/Resume_Arpit_Bharti.pdf">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ backgroundSize: '180%' }}
-              className="p-2 text-lg rounded-md bg-primary text-bg">
-              resume
-            </a>
-          </Link>
-        </div>
-        <Social />
-      </article>
-    </div>
-  );
+export const getStaticProps = async () => {
+  const file = path.resolve(process.cwd(), 'content', 'home.mdx');
+  const source = fs.readFileSync(file, 'utf-8');
+
+  const { code } = await loadMDX(source);
+
+  return { props: { code } };
 };
 
-export default Home;
+const mdxComponents = { ...components, Experience, Projects };
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+export default function Home({ code }: Props) {
+  const Component = useMemo(() => getMDXComponent(code), [code]);
+
+  return (
+    <article className="max-w-[75ch] mx-auto pt-12 pb-28 px-5">
+      <Component components={mdxComponents} />
+    </article>
+  );
+}
