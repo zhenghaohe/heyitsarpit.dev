e3b24e16da31dec7393283c428e63d87f157ad1e
Arpit Bharti <arpitbharti73@gmail.com>
2021-10-16 17:17:03 +0530

    Added listed property to blogs

--- mdx.d.ts
+++ mdx.d.ts
@@ -1,6 +1,9 @@
 interface PostMeta {
   title: string;
+  /** Weather the blog page will be built */
   published: boolean;
+  /** Weather the blog page listed in the website */
+  listed: true;
   date: string;
   description: string;
   tags: string;
